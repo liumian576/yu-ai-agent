590811912dbb37330e7b0da8e57945c25854a1bd
liumian576 <ljx914191957@qq.com>
2025-11-13 20:17:14 +0800

    初始化

---
+++ src/main/java/com/yupi/yuaiagent/demo/invoke/TestApiKey.java
@@ -0,0 +1,10 @@
+package com.yupi.yuaiagent.demo.invoke;
+
+/**
+ * 仅用于测试获取 API Key
+ */
+public interface TestApiKey {
+
+    // 修改为你的 API Key
+    String API_KEY = "sk-0a153c306512474f9d2c13c2709f60a0";
+}
